24e65606b76c53affb3f07ad7ef20700552932d0
Aditya Deshmukh <adityad1405@gmail.com>
2023-09-01 17:21:51 +0530

    Completed exercises 1-6

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,10 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversedString = "";
+    lengthString = string.length;
+    for(let i = lengthString - 1; i >= 0; i--){
+        reversedString += string[i]; 
+    }
+    return reversedString;
 
 };
 
